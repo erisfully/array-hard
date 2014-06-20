9f65c54e004c6214ba24dcc662a68b3eecbf8e11
Ashley <a.clarkem@gmail.com>
2014-06-20 14:52:30 -0600

    its a start

---
+++ coordinate_tictactoe.rb
@@ -0,0 +1,12 @@
+board = [['X', 'O', 'O'], ['X', 'X', 'O'], ['O', 'X', 'O']]
+
+puts board[0].index('X')
+# puts board[1].each do |x|
+#   x.index('X')
+# end
+
+# columns: [0] = y1, [1] = y2, and [2] = y3
+# rows: board[0] = x1, board[1] = x2, board[2] = x3
+
+
+
