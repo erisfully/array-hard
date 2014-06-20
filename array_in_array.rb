34e34faa3da58bd94e7a482eebd5b0cfae941f64
Ashley <a.clarkem@gmail.com>
2014-06-20 13:34:11 -0600

    finished subset checker

---
+++ array_in_array.rb
@@ -0,0 +1,13 @@
+array_1 = [1, 2, 3, 4, 6]
+array_2 = [3, 4, 5]
+array_3 = [4, 2, 1]
+array_4 = [1, 2, 3]
+array_5 = [5, 9, 8]
+
+is_subset = (array_5 - array_1)
+
+if is_subset.count > 0
+  puts false
+else
+  puts true
+end
